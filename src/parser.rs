3ec2717dd8c476e6000034fae246615bc807c458
Tamo <tamo@meilisearch.com>
2021-10-07 21:30:03 +0200

    add some easier to use and more flexible parsers

--- src/parser.rs
+++ src/parser.rs
@@ -4,6 +4,11 @@
 //! Since all the input files are quite small in the advent of code we are putting the full file
 //! into RAM instead of using a BufReader.
 
+use std::{
+    io::{stdin, Read},
+    str::FromStr,
+};
+
 /// Provide an iterator over lines of a `&str` converted as the type you want.
 /// When something can't be parsed it's ignored.
 ///
@@ -22,7 +27,7 @@
 /// assert_eq!(s.next(), Some(2));
 /// assert_eq!(s.next(), None);
 /// ```
-pub fn lines_from_str_as<'a, T: std::str::FromStr>(s: &'a str) -> impl Iterator<Item = T> + 'a {
+pub fn lines_from_str_as<'a, T: FromStr>(s: &'a str) -> impl Iterator<Item = T> + 'a {
     s.lines().filter_map(|l| l.parse::<T>().ok())
 }
 
@@ -37,7 +42,7 @@ pub fn lines_from_str_as<'a, T: std::str::FromStr>(s: &'a str) -> impl Iterator<
 ///     // things
 /// }
 /// ```
-pub fn lines_from_args_as<T: std::str::FromStr>(n: usize) -> impl Iterator<Item = T> {
+pub fn lines_from_args_as<T: FromStr>(n: usize) -> impl Iterator<Item = T> {
     lines_from_args(n).filter_map(|l| l.parse::<T>().ok())
 }
 
@@ -47,7 +52,7 @@ pub fn lines_from_args_as<T: std::str::FromStr>(n: usize) -> impl Iterator<Item 
 ///     // things
 /// }
 /// ```
-pub fn chars_from_file_as<T: std::str::FromStr>(filename: &str) -> impl Iterator<Item = T> {
+pub fn chars_from_file_as<T: FromStr>(filename: &str) -> impl Iterator<Item = T> {
     let file = Box::new(read_file(filename));
     let file = Box::leak(file);
     file.chars().filter_map(|c| c.to_string().parse().ok())
@@ -60,7 +65,7 @@ pub fn chars_from_file_as<T: std::str::FromStr>(filename: &str) -> impl Iterator
 /// }
 /// ```
 /// Usually you'll want to use it with `1` because `0` is the name of your own program
-pub fn chars_from_args_as<T: std::str::FromStr>(n: usize) -> impl Iterator<Item = T> {
+pub fn chars_from_args_as<T: FromStr>(n: usize) -> impl Iterator<Item = T> {
     let filename = get_args(n).expect("give me the path to your program");
     chars_from_file_as(&filename)
 }
@@ -136,7 +141,17 @@ pub fn read_file_from_args(n: usize) -> String {
         .into()
 }
 
-/// Provie the argument at the position `n`:
+/// Read a whole file into a string from `stdin`
+/// ```no_run
+/// let input = aoc::parser::read_file_from_stdin();
+/// ```
+pub fn read_file_from_stdin() -> String {
+    let mut buffer = Vec::new();
+    stdin().read_to_end(&mut buffer).unwrap();
+    std::str::from_utf8(&buffer).unwrap().to_string()
+}
+
+/// Provide the argument at the position `n`:
 /// ```no_run
 /// let exe = aoc::parser::get_args(0).unwrap(); // the name of the executable
 /// let input = match aoc::parser::get_args(1) { // the first argument after the executable
@@ -146,4 +161,57 @@ pub fn read_file_from_args(n: usize) -> String {
 /// ```
 pub fn get_args(n: usize) -> Option<String> {
     std::env::args().nth(n)
+}
+
+/// Provide the input in a `String`.
+/// Will look for your input:
+/// 1. In `stdin`.
+/// 2. In the filename specified in `args[1]`.
+/// 3. In a file named `input` in the current directory.
+/// ```no_run
+/// let input: String = aoc::parser::input();
+/// ```
+pub fn input<T: FromStr>() -> T {
+    if atty::isnt(atty::Stream::Stdin) {
+        read_file_from_stdin()
+    } else if let Some(filename) = get_args(1) {
+        read_file(&filename)
+    } else {
+        std::str::from_utf8(&std::fs::read("input").expect("You need to provide an input. You can either pipe your input in `cargo run`, provide your file name in to `cargo run` or name your input file `input`")).unwrap().to_string()
+    }.parse().ok()
+    .expect("Could not parse the input in the expected type")
+}
+
+/// Provide an [`Iterator`](https://doc.rust-lang.org/stable/std/iter/trait.Iterator.html)
+/// over the input's line parsed into any type implementing
+/// [`FromStr`](https://doc.rust-lang.org/stable/std/str/trait.FromStr.html).
+/// ```no_run
+/// let input: Vec<String> = aoc::parser::lines().collect();
+/// ```
+pub fn lines<T: FromStr>() -> impl Iterator<Item = T> {
+    let input = Box::new(input::<String>());
+    let input = Box::leak(input);
+    input.lines().map(|line| {
+        line.to_owned()
+            .parse()
+            .ok()
+            .expect(&format!("Could not parse the following line: {}", line))
+    })
+}
+
+/// Provide an [`Iterator`](https://doc.rust-lang.org/stable/std/iter/trait.Iterator.html)
+/// over the input's `char` parsed into any type implementing
+/// [`FromStr`](https://doc.rust-lang.org/stable/std/str/trait.FromStr.html).
+/// ```no_run
+/// let input: Vec<u8> = aoc::parser::chars().collect();
+/// ```
+pub fn chars<T: FromStr>() -> impl Iterator<Item = T> {
+    let input = Box::new(input::<String>());
+    let input = Box::leak(input);
+    input.chars().map(|c| {
+        c.to_string()
+            .parse()
+            .ok()
+            .expect(&format!("Could not parse the following char: {}", c))
+    })
 }
