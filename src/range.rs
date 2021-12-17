a45cdeea94d5286ecf10c426eb5d0546c67c06f3
Tamo <tamo@meilisearch.com>
2021-12-17 21:31:58 +0100

    patch the range

--- src/range.rs
+++ src/range.rs
@@ -3,7 +3,7 @@ use anyhow::{ensure, Result};
 use std::fmt;
 use std::ops;
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct Range<I = usize> {
     pub start: Coord<I>,
     current: Coord<I>,
@@ -41,14 +41,16 @@ impl<I: fmt::Debug + Clone + Ord> Range<I> {
     /// assert_eq!(range.contains((15, 15)), true);
     /// assert_eq!(range.contains((14, 16)), false);
     /// assert_eq!(range.contains((9, 12)), false);
+    ///
+    /// let range = Coord::at(20,-10).to(Coord::at(30, -5)).unwrap();
+    /// assert_eq!(range.contains((21, -10)), true);
+    ///
     /// ```
     pub fn contains(&self, other: impl Into<Coord<I>>) -> bool {
         let other: Coord<I> = other.into();
 
-        self.start.x <= other.x
-            && other.x <= self.end.x
-            && self.start.y <= other.y
-            && other.y <= self.end.y
+        (&self.start.x..=&self.end.x).contains(&&other.x)
+            && (&self.start.y..=&self.end.y).contains(&&other.y)
     }
 }
 
