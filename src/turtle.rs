827bce8aa974a2901525df424e8d215fe375b150
Tamo <tamo@meilisearch.com>
2024-12-06 15:08:09 +0100

    implement Hash on the turtle

--- src/turtle.rs
+++ src/turtle.rs
@@ -1,7 +1,7 @@
 use crate::{Coord, Direction, Movement};
 use std::ops::{Add, AddAssign};
 
-#[derive(Debug, Default, Clone, Eq, PartialEq)]
+#[derive(Debug, Default, Clone, Eq, PartialEq, Hash)]
 pub struct Turtle {
     pub coord: Coord<isize>,
     pub facing: Direction,
